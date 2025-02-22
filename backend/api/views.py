d12f987f45e3d241834e676e7cfa9f7d03c38bc6
Lucian Tash <lucian.tash@gatech.edu>
2025-02-22 01:52:54 -0500

    Azure stuff

--- backend/api/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def hello_world(request):
-    return Response({"message": "Hello from Django!"})
