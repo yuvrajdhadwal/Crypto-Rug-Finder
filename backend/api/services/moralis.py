19e44f802f1fb9f47def538f4d2e9645f8e8f07f
amchelapurath2023 <74162023+amchelapurath2023@users.noreply.github.com>
2025-02-22 01:57:17 -0500

    Update moralis.py

--- backend/api/services/moralis.py
+++ backend/api/services/moralis.py
@@ -1,7 +1,7 @@
 import requests
 from moralis import sol_api
 
-api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjYxYjZmYmU5LWQ0NWItNDIzNC1hZGRmLWQwNzgyY2IxZDliNiIsIm9yZ0lkIjoiNDMyOTE1IiwidXNlcklkIjoiNDQ1MzI3IiwidHlwZUlkIjoiMTJiYjcxYzQtNzBjOC00MDI0LWE5YjItOWFiYTFkOGZjYzdiIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3NDAyMDU0NTQsImV4cCI6NDg5NTk2NTQ1NH0._9y_wAuVGB6kyzg9-mjIqHuiWzMrExCB41FeJ76mMZs"
+api_key = ""
 
 params = {
   "network": "mainnet",
@@ -20,7 +20,7 @@ def get_on_chain_info(chain, address):
 
     headers = {
     "Accept": "application/json",
-    "X-API-Key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjYxYjZmYmU5LWQ0NWItNDIzNC1hZGRmLWQwNzgyY2IxZDliNiIsIm9yZ0lkIjoiNDMyOTE1IiwidXNlcklkIjoiNDQ1MzI3IiwidHlwZUlkIjoiMTJiYjcxYzQtNzBjOC00MDI0LWE5YjItOWFiYTFkOGZjYzdiIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3NDAyMDU0NTQsImV4cCI6NDg5NTk2NTQ1NH0._9y_wAuVGB6kyzg9-mjIqHuiWzMrExCB41FeJ76mMZs"
+    "X-API-Key": ""
     }
 
     response = requests.request("GET", url, headers=headers)
