c347d7372e55b54e2efe36ced2ac9bd40fd346ec
Yuvraj <ydhadwal3@gatech.edu>
2025-02-21 22:34:19 -0500

    connected frontend with backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/hello/")
+      .then(response => setMessage(response.data.message))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Frontend</h1>
+      <p>Message from Backend: {message}</p>
     </div>
   );
 }
