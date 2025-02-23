4f859aac8a05f266dd55761767c33693674e2c27
Yuvraj Dhadwal <ydhadwal3@gatech.edu>
2025-02-22 22:10:48 -0500

    added kmeans bot detection

--- backend/api/services/bot_detection.py
+++ backend/api/services/bot_detection.py
@@ -2,20 +2,60 @@ import numpy as np
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from ..models import CryptoTokenSpam
 
 # Common SPAM keywords
 SPAM_KEYWORDS = {
-    'airdrop', '100x', 'pump', 'moon', 'guaranteed', 'free crypto', 'investment',
-    'daily profit', 'easy money', 'DOGE', "diamond", "hold", 
+    'airdrop', '100x', 'pump', 'moon',
+    'profit', 'easy', 'DOGE', "diamond", "hold", 
+    'WAGMI', 'NGMI', 'paper', 'Lambo', 'Lamborghini',
+    'mooning', 'bear', 'bull', 'Shitcoin', 'HODL', 'FOMO',
+    'FUD', 'wen', 'whale', 'p&d',
 }
 
-def extract_features(posts):
+def extract_features(texts):
     analyzer = SentimentIntensityAnalyzer()
     vectorizer = TfidfVectorizer(max_features=500, stop_words="english")
 
     # compute tf-idf matrix
+    tfidf_matrix = vectorizer.fit_transform(texts).toarray()
+
+    data = []
+    for i, text in enumerate(texts):
+        sentiment = analyzer.polarity_scores(text)["compound"]
+        text_length = len(text)
+        num_links = len(re.findall(r'http[s]?://', text))  # Count URLs
+        spam_word_count = sum(1 for word in text.lower().split() if word in SPAM_KEYWORDS)
+
+        # Combine all features
+        features = [
+            text_length, sentiment, num_links, spam_word_count,
+        ] + list(tfidf_matrix[i])
+
+        data.append(features)
+    
+    return np.array(data)
+
+
+def detect_spam(texts, n_clusters=2):
+    """Using k-means clustering to catch potential bot/spam activity"""
+    
+    features = extract_features(texts)
+
+    scaler = StandardScaler()
+    features_scaled = scaler.fit_transform(features)
+
+    kmeans = KMeans(n_clusters=n_clusters, random_state=8152005, n_init=10)
+    clusters = kmeans.fit_predict(features_scaled)
+
+    cluster_means = features_scaled.mean(axis=0)
+    spam_cluster = np.argmax(cluster_means[4])  # Spam keywords as key identifier
+
+    spam_labels = [clusters[i] == spam_cluster for i in range(len(texts))]
+
+    return spam_labels
 
 def compute_bot_activity(posts, comments):
     """Computes percentage of posts and comments are potentially bots/spam"""
--- backend/api/services/sentiment.py
+++
@@ -1,4 +0,0 @@
-from ..models import RedditComment, RedditPost, CryptoTokenSentiment
-from ..views import get_reddit_posts
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
