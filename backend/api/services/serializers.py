e590b9be378a4fa46ddc99d8b491f95876129f32
Lucian Tash <lucian.tash@gatech.edu>
2025-02-23 03:40:08 -0500

    delete everything

--- backend/api/services/serializers.py
+++
@@ -1,8 +0,0 @@
-# backend/api/serializers.py
-from rest_framework import serializers
-from api.models import MarketData
-
-class MarketDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MarketData
-        fields = '__all__'
--- backend/backend/urls.py
+++
@@ -1,7 +0,0 @@
-# backend/api/urls.py
-from django.urls import path, include
-from api.views import market_data_view
-
-urlpatterns = [
-    path('api/', include('api.urls')),
-]
