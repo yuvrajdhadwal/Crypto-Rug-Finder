6d7840bc21f4bc62d14c7e5be3a84f2f29ee6388
Yuvraj Dhadwal <ydhadwal3@gatech.edu>
2025-02-23 08:12:47 -0500

    tesst

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@ const isDevelopment = import.meta.env.MODE === 'development'
 const baseURL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD
 
 const config = {
-    baseURL: baseURL
+    baseURL: 'https://cryptorugfinder-backend.azurewebsites.net/'
 };
 
 export default config;
