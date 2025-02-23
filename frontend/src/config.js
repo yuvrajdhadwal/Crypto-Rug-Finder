49432cc1f19e0a4445a876cfbf0757c5cc024a82
Yuvraj Dhadwal <ydhadwal3@gatech.edu>
2025-02-23 07:20:54 -0500

    added frontend deployment functionality

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,8 @@
+const isDevelopment = import.meta.env.MODE === 'development'
+const baseURL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD
+
 const config = {
-    baseURL: "http://localhost:8000"
+    baseURL: baseURL
 };
 
 export default config;
